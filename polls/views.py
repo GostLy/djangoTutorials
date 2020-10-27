6ba7cb33ce8634ac00797a55d1a9c093f1d61368
GostLy <GostLy@users.noreply.github.com>
2020-10-26 21:50:29 -0400

    django tutorial part 3 complete

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -16,10 +16,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    # try:
+    #    question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
