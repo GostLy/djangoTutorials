41c7287a16bf3636623834a9b7653240b5aec2a9
GostLy <GostLy@users.noreply.github.com>
2020-10-31 23:45:10 -0400

    Finish tutorial part 7

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -16,7 +16,9 @@ class QuestionAdmin(admin.ModelAdmin):
             {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInLine]
-    # fields = ['pub_date', 'question_text']
+    fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 
 admin.site.register(Question, QuestionAdmin)
